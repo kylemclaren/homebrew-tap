c28ebd13e53e61ba8c7bd363e8db31a007681e1e
mclaren <mclaren@fly.io>
2026-09-18 14:11:41 +0000

    jevql 0.2.0

--- Formula/jevql.rb
+++ Formula/jevql.rb
@@ -1,28 +1,28 @@
 class Jevql < Formula
   desc "psql-shaped client that evaluates jev() with TypeSafe against vanilla Postgres"
   homepage "https://github.com/kylemclaren/jevql"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kylemclaren/jevql/releases/download/v0.1.0/jevql_0.1.0_darwin_arm64.tar.gz"
-      sha256 "70721f1d0db6159d9f57142c2806c00ac098652331c749d530db6199f7feed4c"
+      url "https://github.com/kylemclaren/jevql/releases/download/v0.2.0/jevql_0.2.0_darwin_arm64.tar.gz"
+      sha256 "3f1a555facce3d42f3c87abce15805bab926f6ec5d0826eb8012ab74414ef6d0"
     end
     on_intel do
-      url "https://github.com/kylemclaren/jevql/releases/download/v0.1.0/jevql_0.1.0_darwin_amd64.tar.gz"
-      sha256 "6617506f96164d93a4079668fc4f92ffd001ac8cff386867dda83e5586264be6"
+      url "https://github.com/kylemclaren/jevql/releases/download/v0.2.0/jevql_0.2.0_darwin_amd64.tar.gz"
+      sha256 "f47bd00ba977ad59c2963e756933956238bba999b65244efc25fca5698466aa1"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/kylemclaren/jevql/releases/download/v0.1.0/jevql_0.1.0_linux_arm64.tar.gz"
-      sha256 "1c8dc76f8ebc162dd2d22a2eccaa61c8fac23e96ca4201c7d91e68f6fa4e5677"
+      url "https://github.com/kylemclaren/jevql/releases/download/v0.2.0/jevql_0.2.0_linux_arm64.tar.gz"
+      sha256 "716434352b62dc0db4376af9ccdf97a0a20d3ebc29ee20d56ce7a1c9fc5d30bb"
     end
     on_intel do
-      url "https://github.com/kylemclaren/jevql/releases/download/v0.1.0/jevql_0.1.0_linux_amd64.tar.gz"
-      sha256 "4074689cec5925608d5d1f55e5f81fc7a7ce1df4ecdf31a808c3a1ce77fed5a1"
+      url "https://github.com/kylemclaren/jevql/releases/download/v0.2.0/jevql_0.2.0_linux_amd64.tar.gz"
+      sha256 "8087e28e7c24ca17e80410af0664bd883c7be416ce17c5a5eb20b9a45af5db58"
     end
   end
 
